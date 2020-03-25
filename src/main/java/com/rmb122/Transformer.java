d5612ec73bc50e782b5c7f922775f175d027277f
rmb122 <me@rmb122.cn>
2020-03-25 23:30:02 +0800

    Update

--- src/main/java/com/rmb122/Transformer.java
+++ src/main/java/com/rmb122/Transformer.java
@@ -8,14 +8,14 @@ import java.security.ProtectionDomain;
 public class Transformer implements ClassFileTransformer {
     @Override
     public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
-        if (classfileBuffer.length == 112079) {
+        if (classfileBuffer.length == 111989) {
             System.out.println("Detected class, patching...");
             System.out.println(className);
 
             byte[] newBytes = new byte[classfileBuffer.length];
             System.arraycopy(classfileBuffer, 0, newBytes, 0, newBytes.length);
 
-            int[] patch = {31264, 0, 31265, 3, 31292, 0, 31293, 3, 31379, 167, 31380, 90, 31381, 36};
+            int[] patch = {31171, 0, 31172, 3, 31199, 0, 31200, 3, 31286, 167, 31287, 90, 31288, 39};
             for(int i = 0; i < patch.length; i += 2) {
                 newBytes[patch[i]] = (byte) patch[i + 1];
             }
